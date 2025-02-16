ed4a78ff4e969afb0d927d7b48fe9a1e930d92c3
Ronak Bothra <ronakbothraa@gmail.com>
2025-02-16 19:20:16 +0530

    learning javascript

--- codewithmosh/index.js
+++ codewithmosh/index.js
@@ -24,6 +24,27 @@ function Address(street, city, state) {
     this.state = state;
 }
 
-address1 = new Address('a', 'b', 'c');
-address2 = new Address('a', 'b', 'c');
+const addresses = [
+    new Address('a', 'b', 'c'),
+    new Address('d', 'e', 'f'),
+    new Address('a', 'b', 'c'), // Duplicate address
+];
+
+const foundAddress = addresses.find(address => address.street === 'a');
+
+console.log(foundAddress); 
+
+// splice and slice are two different methods
+// splice does not return a new array, it modifies the original array
+// slide return the items as a new array
+
+const number = [-5,1,2,3]
+number.forEach((number) => {
+    if (number >= 0) {
+        console.log(number);
+    }
+})
+const filtered  = number.filter(n => n >= 0)
+console.log(filtered)
+
 
