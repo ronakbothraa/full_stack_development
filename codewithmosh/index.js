00ecee5b494965c54c4fc9a72e5dfbbb2b9a4a1e
Ronak Bothra <ronakbothraa@gmail.com>
2025-02-18 19:10:13 +0530

    learninf javascript

--- codewithmosh/index.js
+++ codewithmosh/index.js
@@ -1,50 +1,21 @@
-const add = {
-    street: '123 Main St',
-    city: 'Springfield',
-    state: 'IL',
-}
-
-function showAddress(address) {
-    for (let key in address) {
-        console.log(key, address[key]);
+const video = {
+    title: 'a',
+    tags: ['a', 'b', 'c'],
+    showTags() {
+        this.tags.forEach(tag => {
+            console.log(this.title, tag);
+        });
     }
 }
 
-function createAddress(street, city, state) {
-    return {
-        street,
-        city,
-        state
-    }
+function s(...args) {
+    return args.reduce((a, b) => {
+        if (Array.isArray(b)) {
+            b = b.reduce((c, d) => {
+                return c+d
+            })
+        }
+        return a+b
+    });
 }
-
-function Address(street, city, state) {
-    this.street = street;
-    this.city = city;
-    this.state = state;
-}
-
-const addresses = [
-    new Address('a', 'b', 'c'),
-    new Address('d', 'e', 'f'),
-    new Address('a', 'b', 'c'), // Duplicate address
-];
-
-const foundAddress = addresses.find(address => address.street === 'a');
-
-console.log(foundAddress); 
-
-// splice and slice are two different methods
-// splice does not return a new array, it modifies the original array
-// slide return the items as a new array
-
-const number = [-5,1,2,3]
-number.forEach((number) => {
-    if (number >= 0) {
-        console.log(number);
-    }
-})
-const filtered  = number.filter(n => n >= 0)
-console.log(filtered)
-
-
+console.log(s(1,2,[3, 45],4,[5,65,2]))
