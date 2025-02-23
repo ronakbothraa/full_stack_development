b8ac59eae536c44ec7625411b6fb328a8df98a0d
Ronak Bothra <ronakbothraa@gmail.com>
2025-02-23 16:31:06 +0530

    prototypical inheritence

--- codewithmosh/js/prototype.js
+++ codewithmosh/js/prototype.js
@@ -1,1 +1,17 @@
- 
+function shape() {
+}
+
+shape.prototype.duplicate = function() {
+    console.log('duplicate')
+}
+
+function circle() {
+    this.radius = 1
+}
+circle.prototype = Object.create(shape.prototype)
+circle.prototype.draw = function() {
+    console.log('draw')
+}
+
+const s = new shape()
+const c = new circle()
