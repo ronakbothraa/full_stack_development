21810e516ac2c4110e5b4134b24101ea1e9c1e99
Ronak Bothra <ronakbothraa@gmail.com>
2025-02-28 02:18:35 +0530

    Game marketplace

--- game-marketplace/src/App.tsx
+++ game-marketplace/src/App.tsx
@@ -1,8 +1,19 @@
-import ListGroup from './component/ListGroup'
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <h1><ListGroup/></h1>
+  return (
+    <Grid templateAreas={"nav nav asdie main"}>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dogerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
- 
+export default App;
