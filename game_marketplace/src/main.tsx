f11a1b4cfbe358b6e2f4d613ab17b7fc3f1c1579
Ronak Bothra <ronakbothraa@gmail.com>
2025-03-01 18:17:01 +0530

    fixing the npx problem by not addressing it

--- game_marketplace/src/main.tsx
+++ game_marketplace/src/main.tsx
@@ -1,10 +1,12 @@
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
 )
