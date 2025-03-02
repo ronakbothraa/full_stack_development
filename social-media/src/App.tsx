a9ca08d76403a6089c0f427d7a823d304959e00a
Ronak Bothra <ronakbothraa@gmail.com>
2025-03-03 03:31:32 +0530

    created signup page

--- social-media/src/App.tsx
+++ social-media/src/App.tsx
@@ -1,6 +1,8 @@
+import AuthLayout from "./_auth/AuthLayout";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import { Home } from "./_root/pages";
+import RootLayout from "./_root/RootLayout";
 import "./globals.css";
 import {Routes, Route} from "react-router-dom";
 
@@ -8,11 +10,14 @@ const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-up" element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
         
-        
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
 
